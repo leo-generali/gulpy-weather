d7c496c67ed800d6ef4102171db72ca3ab55164f
Leo Generali <leogen17@gmail.com>
2017-08-23 17:13:06 -0400

    Got everything working

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,13 @@
 var gulp = require('gulp');
 var inject = require('gulp-inject');
-var webserver = require('gulp-webserver')
+var webserver = require('gulp-webserver');
+
+var htmlclean = require('gulp-htmlclean');
+var cleanCSS = require('gulp-clean-css');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+
+var del = require('del');
 
 var paths = {
 	src: 'src/**/*',
@@ -54,4 +61,54 @@ gulp.task('serve', ['inject'], function() {
 
 gulp.task('watch', ['serve'], function(){
 	gulp.watch(paths.src, ['inject']);
+});
+
+
+///////////////
+//
+// Build workflow
+//
+//////////////
+
+gulp.task('html:dist', function() {
+	return gulp.src(paths.srcHTML)
+		.pipe(htmlclean())
+		.pipe(gulp.dest(paths.dist));
+});
+
+gulp.task('css:dist', function() {
+	return gulp.src(paths.srcCSS)
+		.pipe(concat('style.min.css'))
+		.pipe(cleanCSS())
+		.pipe(gulp.dest(paths.dist));
+});
+
+gulp.task('js:dist', function() {
+	return gulp.src(paths.srcJS)
+		.pipe(concat('script.min.js'))
+		.pipe(uglify())
+		.pipe(gulp.dest(paths.dist));
+});
+
+gulp.task('copy:dist', ['html:dist', 'css:dist', 'js:dist']);
+
+gulp.task('inject:dist', ['copy:dist'], function () {
+	var css = gulp.src(paths.distCSS);
+	var js = gulp.src(paths.distJS);
+	return gulp.src(paths.distIndex)
+		.pipe(inject( css, { relative:true } ))
+		.pipe(inject( js, { relative:true } ))
+		.pipe(gulp.dest(paths.dist));
+});
+
+gulp.task('build', ['inject:dist']);
+
+///////////////
+//
+// Cleaner
+//
+//////////////
+
+gulp.task('clean', function() {
+	del([paths.tmp, paths.dist]);
 });
